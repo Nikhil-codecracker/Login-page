58359f8b2c2aa00f95498da3d4fe86e7c05da9b1
Nikhil-codecracker <nikhilgupta1489@gmail.com>
2020-03-08 01:03:03 +0530

    small changes

--- signup.js
+++ signup.js
@@ -3,7 +3,7 @@ document.getElementById("b2").onclick = function(){
     console.log("nikhil");
     document.getElementById("background").style.transform = "translate(-100%,0)";
     document.getElementById("background").style.borderRadius = "40px 0px 0px 40px";
-    document.getElementById("background").style.transition = "1s";
+    document.getElementById("background").style.transition = "0.5s";
     document.getElementById("signin").style.display = "none";
     document.getElementById("hello").style.display = "none";
     document.getElementById("welcome").style.display = "block";
@@ -12,7 +12,7 @@ document.getElementById("b2").onclick = function(){
 document.getElementById("b3").onclick = function(){
     document.getElementById("background").style.transform = "translate(0%,0)";
     document.getElementById("background").style.borderRadius = "0px 40px 40px 0px";
-    document.getElementById("background").style.transition = "1s";
+    document.getElementById("background").style.transition = "0.5s";
     document.getElementById("signin").style.display = "block";
     document.getElementById("hello").style.display = "block";
     document.getElementById("welcome").style.display = "none";
